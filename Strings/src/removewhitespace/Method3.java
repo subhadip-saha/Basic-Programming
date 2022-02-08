91834a6a357c252366f6f794516932f05a62b34f
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-02-08 10:32:22 +0530

    Removing all the whitespaces from the string

--- Strings/src/removewhitespace/Method3.java
+++ Strings/src/removewhitespace/Method3.java
@@ -1,14 +1,20 @@
 //Java program to remove all the whitespaces from the string.
 //using in-built function
+//user input
 
 package removewhitespace;
+
+import java.util.Scanner;
 
 public class Method3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String str = " Java is a programming language ";
+		System.out.println("Enter a sentence: ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();					//sc.next() only takes first word of the string
+		sc.close();
 		System.out.println("Orginal string is: " + str);
 		String newstr = str.replaceAll(" ", "");			//first one is regex, second one is replacement
 		System.out.println("String after removing spaces: " + newstr);
