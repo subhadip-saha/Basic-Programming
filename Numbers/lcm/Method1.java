7288c723d62d7d730b02fd673e8d6c72839abaa1
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-02-19 07:27:46 +0530

    LCM of two numbers using JAVA

---
+++ Numbers/lcm/Method1.java
@@ -0,0 +1,21 @@
+package lcm;
+
+public class Method1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 8, num2 = 20, h = 1, l;		//h is hcf and l is lcm
+		for(int i = 1; i <= num1; i++)
+		{
+			if(num1 % i == 0 && num2 % i == 0)
+			{
+				h = i;
+			}
+		}
+		
+		l = (num1 * num2)/h;
+		System.out.println("LCM is: " + l);
+	}
+
+}
