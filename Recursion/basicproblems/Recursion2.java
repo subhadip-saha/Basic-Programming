c2a6c28a7813570ee17d081b9b5e26734f91e67b
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-02-26 07:20:40 +0530

    Print numbers from 1 to 5

---
+++ Recursion/basicproblems/Recursion2.java
@@ -0,0 +1,26 @@
+//Print numbers from 1 to 5.
+
+package basicproblems;
+
+public class Recursion2 {
+
+	public static void printNumbers(int n)
+	{
+		if(n == 6)					//base case
+			return;
+		
+		System.out.println(n);		//print
+		
+		printNumbers(n+1);			//recursion, as it is increasing by 1 at each step, so n+1
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 1;
+		printNumbers(n);
+	}
+
+}
+
+
