7dc0ca86382a941ae6fc14b60ae26adcd4f2d20a
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-02-27 07:38:37 +0530

    Basic Patterns
    	print 
    	*
    	*
    	*
    	*
    	print 
    	* * * *
    	print 
    	*
    	*
    	*
    	*
    	* * * *

---
+++ Patterns/basicpatterns/Pattern1.java
@@ -0,0 +1,47 @@
+//some basic beginner patterns
+package basicpatterns;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+/*
+	print 
+	*
+	*
+	*
+	*
+*/
+		//for(int i = 1; i <= 4; i++)
+		//	System.out.println("*");
+		
+//---------------------------------------------------	
+		
+/*
+	print 
+	
+	* * * *
+*/
+		
+		//for(int i = 1; i <= 4; i++)
+		//	System.out.print("*" + " ");
+		
+//---------------------------------------------------		
+/*
+	print 
+	*
+	*
+	*
+	*
+	* * * *
+*/
+		
+		for(int i = 1; i <= 4; i++)
+			System.out.println("*");
+		for(int i = 1; i <= 4; i++)
+			System.out.print("*" + " ");
+	}
+
+	
+}
