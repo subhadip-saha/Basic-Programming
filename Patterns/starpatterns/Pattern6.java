633dfae6d592fbdb854d75d995e02d169d07698b
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-03-03 22:36:27 +0530

    half pyramid

---
+++ Patterns/starpatterns/Pattern6.java
@@ -0,0 +1,37 @@
+/*
+ 	print - half pyramid
+ 	
+ 	* 				or				*
+	**  							* *
+ 	***    							* * *
+ 	**** 							* * * *
+*/
+
+package starpatterns;
+
+import java.util.Scanner;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter how many rows do you want to print?");
+		
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i = 1; i <= r; i++)						//how many rows
+		{
+			for(int j = 1; j <= i; j++)					//in each i-th row, how many columns
+			{
+				System.out.print("*" + " ");		
+				//System.out.print("*");				//print what?
+			}
+			System.out.println();
+		}
+	}
+
+}
