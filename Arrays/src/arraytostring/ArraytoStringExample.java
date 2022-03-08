19a5450292bdb35be80ee03ec3de394830fcd405
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-03-08 12:34:34 +0530

    Array to String using Arrays.toString

--- Arrays/src/arraytostring/ArraytoStringExample.java
+++ Arrays/src/arraytostring/ArraytoStringExample.java
@@ -14,10 +14,19 @@ public class ArraytoStringExample {
 		
 		char[] charArr = new char[] {'H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd'};
 		
+	    int[] intArr = new int[] { 1, 2, 3, 4 };
+	    
+        double[] dblArr = new double[] { 1, 2, 3, 4 };
+        
 		Object[] objArr = new Object[] {1, 2, 3, 4}; 
+		
+
+
 		
 		System.out.println("boolean array: " + Arrays.toString(boolArr));
 		System.out.println("character array: " + Arrays.toString(charArr));
+        System.out.println("Integer Array: "+ Arrays.toString(intArr));
+		System.out.println("Double Array: "+ Arrays.toString(dblArr));
 		System.out.println("object array: " + Arrays.toString(objArr));
 	}
 
