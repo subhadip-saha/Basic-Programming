3a8673763ec6b26a49f34727516d5bd564831dd7
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-03-12 22:29:50 +0530

    perfect pyramid

---
+++ Patterns/starpatterns/Pattern14.java
@@ -0,0 +1,22 @@
+/*
+ 
+ 	print - perfect pyramid
+ 	   
+ 	   *
+ 	  * *
+ 	 * * *
+ 	* * * *
+	   
+*/ 
+
+
+package starpatterns;
+
+public class Pattern14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
